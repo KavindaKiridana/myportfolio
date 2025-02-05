02ca2a039acf9038b6d66cd6591c058fafe0b77b
KavindaKiridana <kavindakiridena@gmail.com>
2025-02-05 09:39:06 +0530

    Update send_email.php

--- codes/send_email.php
+++ codes/send_email.php
@@ -1,34 +1,24 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require "vendor/autoload.php";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
     
-    // Email recipient
-    $to = "kavindakiridena@gmail.com";
-    
-    // Email subject
-    $subject = "New Contact Form Submission from Portfolio";
-    
-    // Email content
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
-    
-    // Email headers
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "X-Mailer: PHP/" . phpversion();
-    
-    // Send email
-    if (mail($to, $subject, $email_content, $headers)) {
-        // Redirect back to contact page with success message
-        header("Location: contact.php?status=success#contactForm");
-    } else {
-        // Redirect back to contact page with error message
-        header("Location: contact.php?status=error#contactForm");
+    try {
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "acidpops76@gmail.com";
+    } catch (Throwable $th) {
+        //throw $th;
     }
+
 } else {
     // If not a POST request, redirect to the contact page
     header("Location: contact.php");
