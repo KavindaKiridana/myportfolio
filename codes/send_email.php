e197f428053738ccfdb2ccf1b4c8eed40618a6d3
KavindaKiridana <kavindakiridena@gmail.com>
2025-02-05 10:23:51 +0530

    Update send_email.php

--- codes/send_email.php
+++ codes/send_email.php
@@ -11,16 +11,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST['message'];
     
     try {
+        $mail = new PHPMailer(true);
+
         $mail->isSMTP();
         $mail->Host = "smtp.gmail.com";
         $mail->SMTPAuth = true;
-        $mail->Username = "acidpops76@gmail.com";
-    } catch (Throwable $th) {
-        //throw $th;
+        $mail->Username = "kavindakiridena@gmail.com";
+        $mail->Password = "ivif zapz khst bdmi";//tell me wht would happne if i accidentally shared this with my email
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        $mail->setFrom($email, $name);  // This sets the sender as the person who filled out the form
+        $mail->addAddress("kavindakiridena@gmail.com", "Jelly");  // This is where the email will be sent to
+
+        $mail->Subject = "myportfolio reply";
+        $mail->Body = "Name: $name\n".
+             "Email: $email\n".
+             "Message: $message";
+
+        if($mail->send()){
+           // Redirect back to contact page with success message
+           header("Location: contact.php?status=success#contactForm");
+        }else{
+           // Redirect back to contact page with error message
+           header("Location: contact.php?status=error#contactForm");
+        }
+    } catch (Exception $e) {
+        header("Location: contact.php?status=error#contactForm");
     }
 
-} else {
-    // If not a POST request, redirect to the contact page
-    header("Location: contact.php");
-}
+} 
 ?>
